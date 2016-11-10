b37a42f7a40ba2c1f2187290115c0c149ac985e8
cleverutd <349856491@qq.com>
2016-11-10 19:22:28 +0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # learning-go
+
+跟着 [Go 指南](https://tour.go-zh.org/list) 学习go的代码
+
+### 运行
+
+主函数在 main/main.go 
+
+### 安装
+
+[下载安装](https://golang.org/doc/install)
+
---
+++ main/main.go
@@ -0,0 +1,58 @@
+package main
+
+// import "fmt"
+import "demo"
+
+func main() {
+	
+	// demo.VariableDeclairation()
+	demo.InitVar()
+	
+	// demo.Print()
+	// demo.ChangeType()
+	// demo.InferType()
+ 
+	// demo.Constants()
+
+	// demo.WhileLoop()
+
+	// fmt.Println(demo.Pow(3,2,10))
+
+	// demo.SwitWithoutCondition()
+
+	// demo.Point()
+
+	// demo.VertexStruct()
+	// demo.AccessStruct()
+	// demo.PointerToStruct()
+	// demo.StructLiterals()
+
+	// demo.Array()
+	// demo.Slice()
+	// demo.MakeSlice()
+	// demo.AppendSlice()
+	
+	// demo.RangeTest()
+	// demo.RangeTest2()
+
+	// demo.MapDemo()
+	// demo.MutatingMap()
+
+	// demo.FunctionClosures()
+
+	// demo.StructMethod()
+	// demo.StructMethod1()
+
+	// demo.PointerRecv()
+
+	// demo.ImplOfInterface()
+    
+
+	// goroutine.GoSay()
+	// goroutine.Channel()
+	// goroutine.BufferChannel()
+	// goroutine.Calfibonacci()
+	// goroutine.Select()
+	// goroutine.SyncMutex()
+}
+
